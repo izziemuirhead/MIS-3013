99653594adcd9d795fd42a6ac00e8e2b570aa829
Izzie Muirhead <izziemuirhead@ou.edu>
2020-09-01 11:50:08 -0500

    Created new repository with in class examples and participation

---
+++ Participations/Participation2_SumOf3Numbers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Participation2_SumOf3Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
