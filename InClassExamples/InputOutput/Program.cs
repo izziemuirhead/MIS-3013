908e55e97eb17ac4524ffd8d1105a119ec4f5866
Izzie Muirhead <izziemuirhead@ou.edu>
2020-09-01 12:13:40 -0500

    Finished the InputOutput in class example

--- InClassExamples/InputOutput/Program.cs
+++ InClassExamples/InputOutput/Program.cs
@@ -1,12 +1,35 @@
-﻿using System;
+﻿
+
+
+
+
+using System;
 
 namespace InputOutput
 {
     class Program
     {
+        const string CLASS_NAME = "MIS 3013";
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // this wont work because it is a constant 
+            //CLASS_NAME = "MIS 3353";
+            //string firstName, lastName;
+            string firstName = "Izzie";
+            string lastName = "Muirhead";
+
+            Console.WriteLine("Please enter your first name >>");
+            firstName = Console.ReadLine(); //getting what the user typed and storing it in th console
+
+            Console.WriteLine("Please enter your last name >>");
+            lastName = Console.ReadLine();
+
+            Console.WriteLine("What is your GPA? >>");
+            string gpaInput = Console.ReadLine();
+            double gpa = Convert.ToDouble(gpaInput);
+
+            Console.WriteLine(firstName + " " + lastName + " is learning "
+            + CLASS_NAME  + " with a GPA of " + gpa.ToString("N1"));
         }
     }
 }
