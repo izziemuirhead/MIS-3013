8a3a2ddc7703bb670a3339c78faba876fe195bda
Izzie Muirhead <izziemuirhead@ou.edu>
2020-09-03 14:25:24 -0500

    Draft of the participation assignment

--- Participations/Participation2_SumOf3Numbers/Program.cs
+++ Participations/Participation2_SumOf3Numbers/Program.cs
@@ -4,10 +4,30 @@ namespace Participation2_SumOf3Numbers
 {
     class Program
     {
-        const double LUCKY_NUMBER = 7.77;
+        const double LUCKY_NUMBER = 7.777;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Pick a number. >>");
+            string response1 = Console.ReadLine();
+            double numOne = Convert.ToDouble(response1);
+
+            Console.WriteLine("Pick another number. >>");
+            String response2 = Console.ReadLine();
+            double numTwo = Convert.ToDouble(response2);
+
+            Console.WriteLine("Pick another number. >>");
+            String response3 = Console.ReadLine();
+            double numThree = Convert.ToDouble(response3);
+
+            double sum = (numOne + numTwo + numThree);
+
+            Console.WriteLine("The sum of your three numbers is" + " " + sum.ToString("N3"));
+
+            double multiplier = (sum * LUCKY_NUMBER);
+
+            Console.WriteLine("the sum of your three numbers multiplied by the constant 7.777 is" + " " + multiplier.ToString("N3"));
+            Console.ReadLine();
+
         }
     }
 }
