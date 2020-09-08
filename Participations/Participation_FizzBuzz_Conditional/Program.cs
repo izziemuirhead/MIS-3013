167501eead6f48edfa45da8ab8eb9d24cb25114d
Izzie Muirhead <izziemuirhead@ou.edu>
2020-09-08 12:03:47 -0500

    Completed the conditional FizzBuzz code.

---
+++ Participations/Participation_FizzBuzz_Conditional/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Participation_FizzBuzz_Conditional
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int num = rnd.Next(1, 101);
+
+            if (num % 3 == 0)
+            {
+                Console.WriteLine("Fizz");
+            }
+            else if (num % 5 == 0)
+            {
+                Console.WriteLine("Buzz");
+            }
+
+            else if (num % 3 == 0 || num % 5 == 0)
+            {
+                Console.WriteLine("FizzBuzz");
+            }
+            else
+            {
+                Console.WriteLine(num);
+            }
+          
+        }
+    }
+}
---
+++ Participations/SumOf3Numbers_IsabelleMuirhead.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Participation2_SumOf3Numbers
+{
+    class Program
+    {
+        const double LUCKY_NUMBER = 7.777;
+        static void Main(string[] args)
+
+            
+        {
+            double numOne = 0;
+            double numTwo = 0;
+            double numThree = 0;
+
+            Console.WriteLine("Pick a number. >>");
+            numOne = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Pick another number. >>");
+            numTwo = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Pick another number. >>");
+            numThree = Convert.ToDouble(Console.ReadLine());
+
+            double sum = (numOne + numTwo + numThree);
+
+            Console.WriteLine("The sum of your three numbers is" + " " + sum.ToString("N3"));
+            Console.ReadLine();
+
+            double product = (sum * LUCKY_NUMBER);
+
+            Console.WriteLine("the sum of your three numbers multiplied by the constant 7.777 is" + " " + product.ToString("N3"));
+            Console.ReadLine();
+
+        }
+    }
+}
