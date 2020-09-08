17a163020b9f65f229e460ee3e57268687d22e7c
Izzie Muirhead <izziemuirhead@ou.edu>
2020-09-08 12:19:49 -0500

    Fixed my conditional FizzBuzz code again.

--- Participations/Participation_FizzBuzz_Conditional/Program.cs
+++ Participations/Participation_FizzBuzz_Conditional/Program.cs
@@ -9,18 +9,19 @@ namespace Participation_FizzBuzz_Conditional
             Random rnd = new Random();
             int num = rnd.Next(1, 101);
 
-            if (num % 3 == 0)
+
+            if (num % 3 == 0 && num % 5 == 0)
+            {
+                Console.WriteLine("FizzBuzz");
+            }
+            else if (num % 3 == 0)
             {
                 Console.WriteLine("Fizz");
             }
+
             else if (num % 5 == 0)
             {
                 Console.WriteLine("Buzz");
-            }
-
-            else if (num % 3 == 0 && num % 5 == 0)
-            {
-                Console.WriteLine("FizzBuzz");
             }
             else
             {
