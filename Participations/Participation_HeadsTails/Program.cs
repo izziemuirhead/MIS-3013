f25983debb1ad694c1bad96ce1970cd522e1455a
Izzie Muirhead <izziemuirhead@ou.edu>
2020-09-10 11:59:27 -0500

    Completed the HeadsTails participation.

---
+++ Participations/Participation_HeadsTails/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Participation_HeadsTails
+{
+    class Program
+    {
+        const string DEVELOPER_NAME = "Isabelle Muirhead";
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pick heads or tails.");
+            string response = Console.ReadLine();
+            
+            Random rnd = new Random();
+            int num = rnd.Next(1, 3);
+
+            if (num == 1)
+            {
+                Console.WriteLine("Heads!");
+            }
+            else
+            {
+                Console.WriteLine("Tails!");
+            }
+            if ((num == 1) && (response == "heads"))
+            {
+                Console.WriteLine("You win!!!!");
+            }
+            else if ((num == 2) && (response == "tails"))
+            {
+                Console.WriteLine("You win!!!!");
+            }
+            else
+            {
+                Console.WriteLine("You lose!!!!");
+            }
+            Console.ReadLine();
+
+            Console.WriteLine(DEVELOPER_NAME);
+        }
+    }
+}
