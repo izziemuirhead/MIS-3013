8b5537789add6ae410b8c4460df379e1a3cccf97
Izzie Muirhead <izziemuirhead@ou.edu>
2020-10-15 12:28:33 -0500

    How to convert from a char to a int.

---
+++ InClassExamples/CharToInt/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharToInt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            char letter = '8';
+            string letterString = letter.ToString();
+            int number = Convert.ToInt32(letterString);
+            Console.WriteLine(number);
+
+            Console.ReadKey();
+
+        }
+    }
+}
