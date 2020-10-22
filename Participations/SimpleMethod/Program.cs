723b920e2d4ead76147af8af9f08a81f0f84c585
Izzie Muirhead <izziemuirhead@ou.edu>
2020-10-22 11:54:58 -0500

    completed the simple methods participation.

---
+++ Participations/SimpleMethod/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string animal = "";
+            
+            Console.WriteLine("Please enter an animal.");  // get animal from user
+            animal = Console.ReadLine();
+
+            string animalSound = speak(animal);  // executing the method
+            Console.WriteLine(animalSound);      // displaying the correct value from the method
+            Console.ReadKey();
+        }
+
+        static string speak(string animal) // create new method with animal input from the main method
+        {
+            string sound = "";
+
+            if (animal == "dog") // assigning animal sounds to each animal
+            {
+                sound = "woof!";
+            }
+            else if (animal == "monkey")
+            {
+                sound = "oo oo ahh ahh!";
+            }
+            else if (animal == "goat")
+            {
+                sound = "behhhhh!";
+            }
+            else
+            {
+                sound = "cant find animal :(";
+            }
+
+            return sound;
+
+
+        }
+    
+
+    }
+}
