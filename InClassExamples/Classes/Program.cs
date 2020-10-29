e6f1e4bd1dca54012f0324e34559ec1438134d74
Izzie Muirhead <izziemuirhead@ou.edu>
2020-10-29 12:31:24 -0500

    Completed the classes in class example.

---
+++ InClassExamples/Classes/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Rectangle r1 = new Rectangle(10, 15);
+            double r1Area = r1.Area();
+            Console.WriteLine($"The area of r1 is {r1Area.ToString("N1")}");
+
+            Rectangle r2 = new Rectangle(7, 8);
+            double r2Area = r2.Area();
+            Console.WriteLine($"The area of r2 is {r2Area.ToString("N1")}");
+
+           // Console.WriteLine($"The area of this rectangle is {new Rectangle(10,15).Area()}");
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ InClassExamples/Classes/Rectangle.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    public class Rectangle
+    {
+        private int width;
+        private int height;
+
+
+
+        /// <summary>
+        /// this is the defualt/empty constructor for the class.
+        /// </summary>
+        public Rectangle()
+        {
+            width = 0;
+            height = 0;
+
+        }
+
+
+        /// <summary>
+        /// this is the overloaded constructor passing in the starting values for width and height
+        /// </summary>
+        /// <param name="width"></param>
+        /// <param name="height"></param>
+        public Rectangle(int w, int h)
+        {
+            this.width = w;
+            this.height = h;
+        }
+
+        public double Area()
+        {
+            double result = width * height;
+
+            return result;
+
+        }
+    }
+}
