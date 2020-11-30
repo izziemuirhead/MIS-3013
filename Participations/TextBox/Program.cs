a513260aff8ccfd86500e4e57f59000adeb5f307
Izzie Muirhead <izziemuirhead@ou.edu>
2020-11-29 19:41:53 -0600

    Completed the TextBox participation.

--- Participations/TextBox/Program.cs
+++ Participations/TextBox/Program.cs
@@ -10,6 +10,24 @@ namespace TextBox
     {
         static void Main(string[] args)
         {
+            TextBox textbox1 = new TextBox();
+            textbox1.Padding = 8;
+            textbox1.BackColor = ConsoleColor.Cyan;
+            textbox1.ForeColor = ConsoleColor.Magenta;
+            textbox1.Text = "Izzie Muirhead";
+
+            textbox1.DisplayText();
+
+            TextBox textbox2 = new TextBox();
+            textbox2.Padding = 3;
+            textbox2.BackColor = ConsoleColor.Yellow;
+            textbox2.ForeColor = ConsoleColor.DarkBlue;
+            textbox2.Text = "I love golden retrievers";
+
+            textbox2.DisplayText();
+
+
+            Console.ReadKey();
         }
     }
 }
---
+++ Participations/TextBox/TextBox.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextBox
+{
+    class TextBox
+    {
+        public int Padding { get; set; }
+        public ConsoleColor BackColor {get; set; }
+        public ConsoleColor ForeColor { get; set; }
+        public string Text { get; set; }
+
+
+        public TextBox()
+        {
+            Padding = 0;
+            BackColor = ConsoleColor.Black;
+            ForeColor = ConsoleColor.White;
+            Text = "";
+        }
+
+        public void DisplayText()
+        {
+            Console.BackgroundColor = BackColor;
+            Console.ForegroundColor = ForeColor;
+            string padding2 = string.Empty;
+            for (int i = 0; i < Padding; i++)
+            {
+                padding2 = padding2 + " ";
+            }
+            Console.WriteLine($"{padding2} {Text} {padding2}");
+        }
+
+    }
+}
